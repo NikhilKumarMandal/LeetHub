0dbcb73ec050b033da3d6cfd9d550eaf6ac61239
Nikhil <kumarnikhil48578@gmail.com>
2025-05-01 22:57:18 +0530

    feat(middleware): add canAccess check middleware

---
+++ src/controllers/problem.controllers.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from "express";
+import { asyncHandler } from "express-strategy";
+
+
+
+export class Problem{
+    create = asyncHandler(async (req: Request, res: Response) => {
+        
+    })
+}
