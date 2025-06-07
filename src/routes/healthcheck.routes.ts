c77625fe8e77b4f02dbce14077c3d149b6f70d35
Nikhil <kumarnikhil48578@gmail.com>
2025-06-07 18:42:06 +0530

    fix: health chheck change method from post to get

--- src/routes/healthcheck.routes.ts
+++ src/routes/healthcheck.routes.ts
@@ -5,6 +5,6 @@ const router = express.Router();
 
 const healthCheck = new HealthCheck();
 
-router.post("/health-check", healthCheck.heathCheck);
+router.get("/health-check", healthCheck.heathCheck);
 
 export default router;
