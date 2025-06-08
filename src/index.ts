9335c6d3cdada38ef84cd19797eb9fd5d7034cde
Nikhil <kumarnikhil48578@gmail.com>
2025-06-08 09:53:24 +0530

    fix: change url for cors

--- src/index.ts
+++ src/index.ts
@@ -31,7 +31,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"],
+    origin: ["https://www.leethub.tech"],
   },
 });
 
@@ -44,7 +44,7 @@ app.use(express.urlencoded({ extended: true, limit: "116kb" }));
 app.use(express.static("public"));
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["https://www.leethub.tech"],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     credentials: true,
   })
